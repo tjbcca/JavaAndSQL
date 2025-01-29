93bfa9e4aefc1cf7735f9caea26f4c3dd5b1f00d
Tanner Jones <tjones25@basecampcodingacademy.org>
2025-01-29 10:36:42 -0600

    optionclass

--- src/Main.java
+++ src/Main.java
@@ -16,13 +16,7 @@ public class Main {
              Scanner scanner = new Scanner(System.in)) {
 
             while (true) {
-                System.out.println("Library System Menu:");
-                System.out.println("1. View all books");
-                System.out.println("2. Search for a book");
-                System.out.println("3. Add a book");
-                System.out.println("4. Remove a book");
-                System.out.println("5. Exit");
-                System.out.print("Choose an option: ");
+                Options.main();
                 int choice = scanner.nextInt();
                 scanner.nextLine();  // Consume newline
 
---
+++ src/Options.java
@@ -0,0 +1,11 @@
+public class Options {
+    public static void main() {
+        System.out.println("Library System Menu:");
+        System.out.println("1. View all books");
+        System.out.println("2. Search for a book");
+        System.out.println("3. Add a book");
+        System.out.println("4. Remove a book");
+        System.out.println("5. Exit");
+        System.out.print("Choose an option: ");
+    }
+}
